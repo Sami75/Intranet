ef2a3ae31370e1c35e3d07d262a8006213fe1e63
Gerry <Gerry@PC-Eymric>
2018-10-24 10:36:59 +0200

    Création du fichier fxml

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,6 +1,7 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
@@ -11,6 +12,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			BorderPane root = new BorderPane();
+			FXMLLoader fxmlLoader = new FXMLLoader( getClass().getResource("SampleConnexion.fxml") );
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
