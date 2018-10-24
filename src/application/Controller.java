fdb31d8851272525b29e595729556d2fdc3841c3
Gerry <Gerry@PC-Eymric>
2018-10-24 14:11:10 +0200

    Agencement de l'affichage de l'application 'Accueil + Connexion' & Ajout des différentes méthodes.

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -1,5 +1,53 @@
 package application;
 
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URL;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+
 public class Controller {
 
+	@FXML
+	private AnchorPane anchor;
+	@FXML
+	private AnchorPane menu;
+	
+	public void connexion() throws IOException {
+		AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("SampleAccueil.fxml"));
+		anchor.getChildren().setAll(root);
+		System.out.println("La connexion a été effectuée");
+	}
+	
+	public void menu1() throws IOException {
+		System.out.println("Click sur le menu 1");
+		menu.getChildren().clear();
+		AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("SampleMenu.fxml"));
+		menu.getChildren().setAll(root);
+	}
+	
+	public void menu2() throws IOException {
+		System.out.println("Click sur le menu 2");
+		menu.getChildren().clear();
+		AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("SampleMenu2.fxml"));
+		menu.getChildren().setAll(root);
+	}
+	
+	public void menu3() throws IOException {
+		System.out.println("Click sur le menu 3");
+		menu.getChildren().clear();
+		AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("SampleMenu3.fxml"));
+		menu.getChildren().setAll(root);
+	}
+	
+	public void menu4() {
+		System.out.println("Click sur le menu 4");
+		try {
+		    Desktop.getDesktop().browse(new URL("http://licencelerebours.fr/owncloud/index.php/login").toURI());
+		} catch (Exception e) {
+		    e.printStackTrace();
+		}
+	}
 }
--- src/application/Main.java
+++ src/application/Main.java
@@ -1,19 +1,19 @@
 package application;
 	
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.fxml.FXMLLoader;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			FXMLLoader fxmlLoader = new FXMLLoader( getClass().getResource("SampleConnexion.fxml") );
-			Scene scene = new Scene(root,400,400);
+			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("SampleConnexion.fxml"));
+			Scene scene = new Scene(root,800,600);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
